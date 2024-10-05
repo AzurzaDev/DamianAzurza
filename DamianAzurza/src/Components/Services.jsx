cc244795116005b9aa70d0c6278eda49d2ee3d18
mlobeto <mercedeslobeto@gmail.com>
2024-10-04 22:26:19 -0300

    serv

--- DamianAzurza/src/Components/Services.jsx
+++ DamianAzurza/src/Components/Services.jsx
@@ -2,21 +2,19 @@ import { IoMdMusicalNote } from "react-icons/io";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { PiUserSoundFill } from "react-icons/pi";
 import { FaPauseCircle } from "react-icons/fa";
-import debajoDeProyectos from '../assets/debajoDeProyectos.png'
 
 const Services = () => {
   return (
-
-
-    <div className="bg-gray-100 py-16">
-<img
+    <div className="bg-gray-100 py-16 -mt-16 ">
+      <img
         src="src/assets/debajoDeProyectos.png"
         alt="Imagen Fija"
         className="w-full h-full object-cover"
       />
       <h2 className="text-3xl font-semibold text-center mb-8">Servicios</h2>
       <p className="text-center text-gray-500 mb-12">
-        Figma lorem component variant main layer. Create scrolling team bold prototype background.
+        Figma lorem component variant main layer. Create scrolling team bold
+        prototype background.
       </p>
 
       <div className="flex flex-wrap justify-center gap-8">
@@ -25,7 +23,8 @@ const Services = () => {
           <IoMdMusicalNote className="text-6xl text-gray-600 mb-4" />
           <h3 className="text-xl font-semibold">Dirección Musical</h3>
           <p className="text-gray-500 mt-2">
-            Componente de ejemplo que describe la dirección musical de manera breve.
+            Componente de ejemplo que describe la dirección musical de manera
+            breve.
           </p>
         </div>
 
@@ -34,7 +33,8 @@ const Services = () => {
           <AiFillPlayCircle className="text-6xl text-gray-600 mb-4" />
           <h3 className="text-xl font-semibold">Producción Musical</h3>
           <p className="text-gray-500 mt-2">
-            Componente de ejemplo que describe la producción musical de manera breve.
+            Componente de ejemplo que describe la producción musical de manera
+            breve.
           </p>
         </div>
 
@@ -43,7 +43,8 @@ const Services = () => {
           <PiUserSoundFill className="text-6xl text-gray-600 mb-4" />
           <h3 className="text-xl font-semibold">Solista</h3>
           <p className="text-gray-500 mt-2">
-            Componente de ejemplo que describe el servicio de solista de manera breve.
+            Componente de ejemplo que describe el servicio de solista de manera
+            breve.
           </p>
         </div>
 
@@ -52,7 +53,8 @@ const Services = () => {
           <FaPauseCircle className="text-6xl text-gray-600 mb-4" />
           <h3 className="text-xl font-semibold">Duetos</h3>
           <p className="text-gray-500 mt-2">
-            Componente de ejemplo que describe el servicio de duetos de manera breve.
+            Componente de ejemplo que describe el servicio de duetos de manera
+            breve.
           </p>
         </div>
       </div>
