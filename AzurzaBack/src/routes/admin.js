eddba3f9e641f3c1aeea8442efd40c3e3b5c1de4
mlobeto <mercedeslobeto@gmail.com>
2024-10-05 11:20:27 -0300

    ahora imagenes se importan hasta actualizar cloudinary

--- AzurzaBack/src/routes/admin.js
+++ AzurzaBack/src/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-// Ruta protegida solo para administradores autenticados
+
 router.get('/', authMiddleware, (req, res) => {
   res.status(200).json({ message: 'Bienvenido al panel de administración' });
 });
--- AzurzaBack/src/routes/contact.js
+++ AzurzaBack/src/routes/contact.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
 
-// Ruta para crear un nuevo contacto
+
 router.post('/', contactController.createContact);
 
-// Ruta para obtener todos los contactos (opcional)
+
 router.get('/', contactController.getAllContacts);
 
 module.exports = router;
