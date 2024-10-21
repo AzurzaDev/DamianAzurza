07e2f3234f1c8ac60809629d9eae6a781d933197
yanirc1981 <cerzurc@hotmail.com>
2024-10-20 22:22:45 -0300

    toast en contacto

--- DamianAzurza/src/App.jsx
+++ DamianAzurza/src/App.jsx
@@ -3,32 +3,45 @@ import Landing from "./Components/Landing";
 import Panel from "./Components/Admin/Panel";
 import AgendaShows from "./Components/Admin/AgendaShows";
 import PhotosGallery from "./Components/PhotosGallery";
-//import ProtectedRoutes from './utils/ProtectedRoutes';
+// import ProtectedRoutes from './utils/ProtectedRoutes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';  // Asegúrate de importar los estilos
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/fotos" element={<PhotosGallery/>}/>
-      {/* Ruta protegida: solo los administradores pueden ver el Panel */}
-      <Route
-        path="/panel"
-        element={
-          // <ProtectedRoutes>
-          <Panel />
-          // </ProtectedRoutes>
-        }
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        pauseOnFocusLoss
+        onClose={() => toast.dismiss()}
       />
-
-      <Route
-        path="/panel/shows"
-        element={
-          // <ProtectedRoutes>
-          <AgendaShows />
-          // </ProtectedRoutes>
-        }
-      />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/fotos" element={<PhotosGallery />} />
+        {/* Ruta protegida: solo los administradores pueden ver el Panel */}
+        <Route
+          path="/panel"
+          element={
+            // <ProtectedRoutes>
+            <Panel />
+            // </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/panel/shows"
+          element={
+            // <ProtectedRoutes>
+            <AgendaShows />
+            // </ProtectedRoutes>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
