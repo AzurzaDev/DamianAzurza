f922e8021b9241a12436116f804b02711c7bf591
mlobeto <mercedeslobeto@gmail.com>
2024-11-04 09:37:45 -0300

    photos

--- DamianAzurza/src/Components/PhotosGallery.jsx
+++ DamianAzurza/src/Components/PhotosGallery.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { motion } from 'framer-motion';
-import navbar2 from "../assets/debajoSlider.png";
+import navbar2 from "../assets/otroSlider.png";
 import Modal from 'react-modal';
 import './PhotoGallery.css';
 
@@ -50,72 +50,71 @@ const PhotosGallery = () => {
 
   return (
     <div className="bg-white">
-      <div
-        className="relative w-full h-64 bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${navbar2})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col md:flex-row items-center justify-between p-4">
-          <Link to="/">
-            <img src="LogoNavbar.png" alt="Logo" className="w-12 h-12 md:w-16 md:h-16 mb-2 md:mb-0" />
-          </Link>
-          <h1 className="text-white text-3xl md:text-5xl font-bold text-center w-full mb-2 md:mb-0"></h1>
-          {/* <div className="flex space-x-4 text-white">
-            <a href="#"><FiInstagram size={30} /></a>
-            <a href="#"><FiYoutube size={30} /></a>
-            <a href="#"><FiFacebook size={30} /></a>
-          </div> */}
+<div className="relative w-full">
+  <img
+    src={navbar2}
+    alt="Banner"
+    className="w-full h-auto object-cover"
+  />
+  <div className="absolute inset-0 bg-black opacity-50"></div>
+  <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between p-4 max-w-screen-xl mx-auto">
+    <Link to="/">
+      <img src="LogoNavbar.png" alt="Logo" className="w-40 h-40 sm:w-82 sm:h-8 md:w-8 md:h-8" />
+    </Link>
+    {/* Iconos de redes sociales a la derecha */}
+    <div className="flex space-x-4 text-white mr-6 mt-9   ">
+      <a href="#" aria-label="Instagram"><FiInstagram size={30} /></a>
+      <a href="#" aria-label="YouTube"><FiYoutube size={30} /></a>
+      <a href="#" aria-label="Facebook"><FiFacebook size={30} /></a>
+    </div>
+  </div>
+</div>
+
+
+  <div className="container mx-auto px-4 py-8">
+    <div className="photo-gallery container mx-auto px-4 py-8">
+      {visibleImages.map((image, index) => (
+        <motion.div
+          key={index}
+          className={`relative w-full h-auto cursor-pointer ${
+            index % 4 === 0 || index % 4 === 2 ? 'wide-image' : ''
+          }`}
+          onClick={() => openModal(index)}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+        >
+          <img
+            src={image}
+            alt={`Image ${index + 1}`}
+            className="w-full h-full object-cover rounded-md"
+          />
+        </motion.div>
+      ))}
+    </div>
+  </div>
+
+  {selectedImage !== null && (
+    <Modal
+      isOpen={selectedImage !== null}
+      onRequestClose={closeModal}
+      contentLabel="Imagen seleccionada"
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
+      overlayClassName="Overlay"
+      ariaHideApp={false}
+    >
+      <div className="relative" onClick={closeModal}>
+        <button onClick={closeModal} className="absolute top-2 right-2 text-white text-3xl font-bold">&times;</button>
+        <img src={allImages[selectedImage]} alt={`Image ${selectedImage + 1}`} className="max-h-screen max-w-full object-contain" onClick={(e) => e.stopPropagation()} />
+        <div className="absolute inset-0 flex items-center justify-between px-4">
+          <button onClick={(e) => { e.stopPropagation(); prevImage(); }} className="text-white text-3xl">&#10094;</button>
+          <button onClick={(e) => { e.stopPropagation(); nextImage(); }} className="text-white text-3xl">&#10095;</button>
         </div>
       </div>
-
-      <div className="container mx-auto px-4 py-8">
-      <div className="photo-gallery container mx-auto px-4 py-8">
-  {visibleImages.map((image, index) => (
-    <motion.div
-      key={index}
-      className={`relative w-full h-auto cursor-pointer ${
-        index % 4 === 0 || index % 4 === 2 ? 'wide-image' : ''
-      }`}
-      onClick={() => openModal(index)}
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5, delay: index * 0.1 }}
-    >
-      <img
-        src={image}
-        alt={`Image ${index + 1}`}
-        className="w-full h-full object-cover rounded-md"
-      />
-    </motion.div>
-  ))}
+    </Modal>
+  )}
 </div>
-      </div>
 
-      {selectedImage !== null && (
-        <Modal
-          isOpen={selectedImage !== null}
-          onRequestClose={closeModal}
-          contentLabel="Imagen seleccionada"
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
-          overlayClassName="Overlay"
-          ariaHideApp={false}
-        >
-          <div className="relative" onClick={closeModal}>
-            <button onClick={closeModal} className="absolute top-2 right-2 text-white text-3xl font-bold">&times;</button>
-            <img src={allImages[selectedImage]} alt={`Image ${selectedImage + 1}`} className="max-h-screen max-w-full object-contain" onClick={(e) => e.stopPropagation()} />
-            <div className="absolute inset-0 flex items-center justify-between px-4">
-              <button onClick={(e) => { e.stopPropagation(); prevImage(); }} className="text-white text-3xl">&#10094;</button>
-              <button onClick={(e) => { e.stopPropagation(); nextImage(); }} className="text-white text-3xl">&#10095;</button>
-            </div>
-          </div>
-        </Modal>
-      )}
-    </div>
   );
 };
 
