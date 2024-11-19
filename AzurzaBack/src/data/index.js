183131ab0a81149339b1fb56b7cbd3cbd7cb472f
yanirc1981 <cerzurc@hotmail.com>
2024-11-19 10:30:50 -0300

    se crean con fecha correcta

--- AzurzaBack/src/data/index.js
+++ AzurzaBack/src/data/index.js
@@ -15,13 +15,15 @@ const sequelize = new Sequelize(
   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    native: false,  // lets Sequelize know we can use pg-native for ~30% more speed
+    timezone: '-03:00', // Configura la zona horaria GMT-3 (Argentina)
   }
 );
 //-------------------------------------CONFIGURACION PARA EL DEPLOY---------------------------------------------------------------------
 // const sequelize = new Sequelize(DB_DEPLOY , {
 //       logging: false, // set to console.log to see the raw SQL queries
 //       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//       timezone: '-03:00', 
 //     }
 //   );
 
